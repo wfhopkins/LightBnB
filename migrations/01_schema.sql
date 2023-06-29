40588f59632e67e50bde8a597b275ca8508239f6
wfhopkins <will.hop@gmail.com>
2023-06-28 22:48:27 -0700

    add seeds folder, insert fake data into tables

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -37,7 +37,7 @@ CREATE TABLE properties (
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-  start_datae DATE NOT NULL,
+  start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
