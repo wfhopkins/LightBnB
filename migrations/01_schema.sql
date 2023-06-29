ec9419b1e0749cf5b37cdfec7754f5a5894f7a8b
wfhopkins <will.hop@gmail.com>
2023-06-28 22:02:05 -0700

    tables successfuly created with params

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS resdervations CASCADE;
 DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
 DROP TABLE IF EXISTS property_reviews CASCADE;
 
 
@@ -11,38 +11,44 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE reservations (
-  id SERIAL PRIMARY KEY NOT NULL,
-  start_datae DATE,
-  end_date DATE,
-  property_id INTEGER REFERENCES properties(id) = properties.id,
-  guest_id INTEGER REFERENCES users(id) = users.id,
-);
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
-  owner_id INTEGER REFERENCES owners(id) = owners.id,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  thumbnail_photo_url VARCHAR(255),
-  cover_photo_url VARCHAR(255),
-  cost_per_night INTEGER NOT NULL,
-  parking_spaces INTEGER NOT NULL,
-  number_of_bathrooms INTEGER NOT NULL,
-  number_of_bedrooms INTEGER NOT NULL,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  parking_spaces INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+  
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
-  active BOOLEAN  TRUE
+  
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
+
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_datae DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  guest_id INTEGER REFERENCES users(id) = users.id,
-  property_id INTEGER REFERENCES properties(id) = properties.id,
-  reservation_id INTEGER REFERENCES reservations(id) = reservations.id,
-  rating SMALLINT,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
