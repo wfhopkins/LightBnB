b51587962fc582ac07c59b00764898e04ba84c52
wfhopkins <will.hop@gmail.com>
2023-06-29 19:00:01 -0700

    get getAllReservations to load resverations for each user. git status  user reservations persist after logout...

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -75,8 +75,26 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+    SELECT *
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id, property_reviews.id
+    ORDER BY $1
+    LIMIT $2;`, [guest_id, limit])
+  .then((result) => {
+    console.log(result.rows);
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
+
+//SELECTG statement from previous iteration ^^^
+// id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
 
 /// Properties
 
